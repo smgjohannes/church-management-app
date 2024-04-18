5940f2313a462a67adb160bed2c2d606119cea0a
smgjohannes <smgjohannes@gmail.com>
2024-04-18 17:04:08 +0200

    backends

--- src/components/Member/MemberForm.js
+++ src/components/Member/MemberForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './MemberForm.css';
 
@@ -9,162 +9,179 @@ function MemberForm() {
   const [age, setAge] = useState('');
   const [email, setEmail] = useState('');
   const [image, setImage] = useState(null);
+  const [dateOfBirth, setDateOfBirth] = useState('');
+  const [baptised, setBaptised] = useState('');
+  const [memberOf, setMemberOf] = useState('');
+  const [localChurch, setLocalChurch] = useState('');
+  const [fromDate, setFromDate] = useState('');
+  const [father, setFather] = useState('');
+  const [mother, setMother] = useState('');
+  const [uploadSuccess, setUploadSuccess] = useState(false);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+
+    const formData = new FormData();
+    formData.append('id', id);
+    formData.append('name', name);
+    formData.append('surname', surname);
+    formData.append('age', age);
+    formData.append('email', email);
+    formData.append('image', image);
+    formData.append('dateOfBirth', dateOfBirth);
+    formData.append('baptised', baptised);
+    formData.append('memberOf', memberOf);
+    formData.append('localChurch', localChurch);
+    formData.append('fromDate', fromDate);
+    formData.append('father', father);
+    formData.append('mother', mother);
+
     try {
-      const formData = new FormData();
-      formData.append('id', id);
-      formData.append('name', name);
-      formData.append('surname', surname);
-      formData.append('age', age);
-      formData.append('email', email);
-      formData.append('image', image);
-      formData.append(
-        'dateOfBirth',
-        document.getElementById('dateOfBirth').value
+      const response = await axios.post(
+        'http://localhost:3000/api/members',
+        formData,
+        {
+          headers: {
+            'Content-Type': 'multipart/form-data',
+          },
+        }
       );
-      formData.append('baptised', document.getElementById('baptised').value);
-      formData.append('memberOf', document.getElementById('memberOf').value);
-      formData.append(
-        'localChurch',
-        document.getElementById('localChurch').value
-      );
-      formData.append('fromDate', document.getElementById('fromDate').value);
-      formData.append('father', document.getElementById('father').value);
-      formData.append('mother', document.getElementById('mother').value);
 
-      await axios.post('/api/members', formData, {
-        headers: {
-          'Content-Type': 'multipart/form-data',
-        },
-      });
-
-      // Clear form fields after successful submission
-      setId('');
-      setName('');
-      setSurname('');
-      setAge('');
-      setEmail('');
-      setImage(null);
+      if (response.status === 201) {
+        console.log('Member added successfully');
+        setUploadSuccess(true);
+        // Reset all fields after successful submission
+        setId('');
+        setName('');
+        setSurname('');
+        setAge('');
+        setEmail('');
+        setImage(null);
+        setDateOfBirth('');
+        setBaptised('');
+        setMemberOf('');
+        setLocalChurch('');
+        setFromDate('');
+        setFather('');
+        setMother('');
+      } else {
+        console.error('Error adding member:', response.statusText);
+      }
     } catch (error) {
-      console.error('Error saving member details:', error);
+      console.error('Error adding member:', error);
     }
   };
+
+  const handleImageChange = (event) => {
+    setImage(event.target.files[0]);
+  };
+
+  useEffect(() => {
+    if (uploadSuccess) {
+      const timer = setTimeout(() => {
+        setUploadSuccess(false);
+      }, 10000);
+
+      return () => clearTimeout(timer);
+    }
+  }, [uploadSuccess]);
+
   return (
-    <div>
-      <h2>Add New Member</h2>
-      <div className='form_MAinContainer'>
-        <form className='form_MainContainer' onSubmit={handleSubmit}>
-          <div className='form-row'>
-            <div className='form-group'>
-              <label htmlFor='id'>Id:</label>
-              <input
-                type='number'
-                id='id'
-                value={id}
-                onChange={(e) => setId(e.target.value)}
-                required
-              />
-            </div>
-            <div className='form-group'>
-              <label htmlFor='name'>Name:</label>
-              <input
-                type='text'
-                id='name'
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-                required
-              />
-            </div>
-          </div>
-          <div className='form-row'>
-            <div className='form-group'>
-              <label htmlFor='surname'>Surname:</label>
-              <input
-                type='text'
-                id='surname'
-                value={surname}
-                onChange={(e) => setSurname(e.target.value)}
-                required
-              />
-            </div>
-            <div className='form-group'>
-              <label htmlFor='age'>Age:</label>
-              <input
-                type='number'
-                id='age'
-                value={age}
-                onChange={(e) => setAge(e.target.value)}
-                required
-              />
-            </div>
-          </div>
-          <div className='form-row'>
-            <div className='form-group'>
-              <label htmlFor='email'>Email:</label>
-              <input
-                type='email'
-                id='email'
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                required
-              />
-            </div>
-            <div className='form-group'>
-              <label htmlFor='image'>Image:</label>
-              <input
-                type='file'
-                id='image'
-                accept='image/*'
-                onChange={(e) => setImage(e.target.files[0])}
-                required
-              />
-            </div>
-          </div>
-          <div className='form-row'>
-            <div className='form-group'>
-              <label htmlFor='dateOfBirth'>Date of Birth:</label>
-              <input type='date' id='dateOfBirth' />
-            </div>
-            <div className='form-group'>
-              <label htmlFor='baptised'>Baptised:</label>
-              <input type='date' id='baptised' />
-            </div>
-          </div>
-          <div className='form-row'>
-            <div className='form-group'>
-              <label htmlFor='memberOf'>Member Of:</label>
-              <input type='text' id='memberOf' />
-            </div>
-            <div className='form-group'>
-              <label htmlFor='localChurch'>Local Church:</label>
-              <input type='text' id='localChurch' />
-            </div>
-          </div>
-          <div className='form-row'>
-            <div className='form-group'>
-              <label htmlFor='fromDate'>From:</label>
-              <input type='date' id='fromDate' />
-            </div>
-            <div className='form-group'>
-              <label htmlFor='father'>Father:</label>
-              <input type='text' id='father' />
-            </div>
-          </div>
-          <div className='form-row'>
-            <div className='form-group'>
-              <label htmlFor='mother'>Mother:</label>
-              <input type='text' id='mother' />
-            </div>
-          </div>
-          <div className='centerButton'>
-            <button className='submitButton' type='submit'>
-              Submit
-            </button>
-          </div>
-        </form>
-      </div>
+    <div className='AdminContainer'>
+      <h1>Add New Member</h1>
+      <form onSubmit={handleSubmit} className='form-container'>
+        <label>Id:</label>
+        <input
+          type='number'
+          value={id}
+          onChange={(e) => setId(e.target.value)}
+          required
+        />
+        <label>Name:</label>
+        <input
+          type='text'
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          required
+        />
+        <label>Surname:</label>
+        <input
+          type='text'
+          value={surname}
+          onChange={(e) => setSurname(e.target.value)}
+          required
+        />
+        <label>Age:</label>
+        <input
+          type='number'
+          value={age}
+          onChange={(e) => setAge(e.target.value)}
+          required
+        />
+        <label>Email:</label>
+        <input
+          type='email'
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        <label>Image:</label>
+        <input
+          type='file'
+          accept='image/*'
+          name='image'
+          onChange={handleImageChange}
+          required
+        />
+        <label>Date of Birth:</label>
+        <input
+          type='date'
+          value={dateOfBirth}
+          onChange={(e) => setDateOfBirth(e.target.value)}
+        />
+        <label>Baptised:</label>
+        <input
+          type='date'
+          value={baptised}
+          onChange={(e) => setBaptised(e.target.value)}
+        />
+        <label>Member Of:</label>
+        <input
+          type='text'
+          value={memberOf}
+          onChange={(e) => setMemberOf(e.target.value)}
+        />
+        <label>Local Church:</label>
+        <input
+          type='text'
+          value={localChurch}
+          onChange={(e) => setLocalChurch(e.target.value)}
+        />
+        <label>From:</label>
+        <input
+          type='date'
+          value={fromDate}
+          onChange={(e) => setFromDate(e.target.value)}
+        />
+        <label>Father:</label>
+        <input
+          type='text'
+          value={father}
+          onChange={(e) => setFather(e.target.value)}
+        />
+        <label>Mother:</label>
+        <input
+          type='text'
+          value={mother}
+          onChange={(e) => setMother(e.target.value)}
+        />
+        <button className='submitButton' type='submit'>
+          Submit
+        </button>
+      </form>
+      {uploadSuccess && (
+        <p className='success-message'>Member added successfully!</p>
+      )}
     </div>
   );
 }
